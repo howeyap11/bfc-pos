48d38ff911243ab7c16e3218dc73d5a0b71689e5
howeandrelukeyap <howeandrelukeyap@gmail.com>
2026-03-12 16:17:42 +0800

    deploy fix 2

--- apps/cloud-api/prisma/seed.ts
+++ apps/cloud-api/prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../src/generated/prisma/index.js";
 import { hashPassword } from "../src/lib/password.js";
 
 const prisma = new PrismaClient();
